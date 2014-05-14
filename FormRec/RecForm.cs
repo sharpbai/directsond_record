e49027e075315ccf9905af5e56ee1e6954c7d7aa
sharpbai <tian.bai@duobei.com>
2014-05-14 14:44:15 +0800

    初始版本

---
+++ FormRec/RecForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using libDxRec;
+
+namespace FormRec
+{
+    public partial class RecForm : Form
+    {
+        Recoder recoder;
+
+        public RecForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recoder = new Recoder();
+            recoder.SetFileName("D://rec.wav");
+            recoder.RecStart();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            recoder.RecStop();
+        }
+
+        private void RecForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ TestRec/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using libDxRec;
+
+namespace TestRec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recoder recoder = new Recoder();
+            recoder.SetFileName("D://rec.wav");
+            Console.WriteLine("现在开始录制，按回车结束录制");
+            recoder.RecStart();
+            Console.Read();
+            recoder.RecStop();
+        }
+    }
+}
